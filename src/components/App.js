61406f40d8fb919ab892d4ad0582e974f39a7710
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-10-18 21:55:15 +1100

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import { useEffect, useState } from "react";
+
+function App() {
+        const [doggoImg, setDoggoImg] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then(data => {
+                setDoggoImg(data.message);
+                setIsLoaded(true);
+            })
+    }, []); //only run once, onMount
+
+    //if the doggo image has not yet been loaded, show a loading message
+    if (!isLoaded)
+        return <p>Loading...</p>
+    return (
+        <img src={doggoImg} alt="A Random Dog"/>
+    )
+  }
+  
+  export default App;
